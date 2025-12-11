099b2d468cb3d7b064c962f7159ee0a3dd4168c1
shramana263 <shramana652@gmail.com>
2025-12-11 23:48:55 +0530

    add implementation for counting covered buildings

---
+++ daily_challanges/count_cover_building/CoverBuilding.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class CoverBuilding {
+    public int countCoveredBuildings(int n, int[][] buildings) {
+        int[] maxRow = new int[n + 1];
+        int[] minRow = new int[n + 1];
+        int[] maxCol = new int[n + 1];
+        int[] minCol = new int[n + 1];
+
+        Arrays.fill(minRow, n + 1);
+        Arrays.fill(minCol, n + 1);
+
+        for (int[] p : buildings) {
+            int x = p[0];
+            int y = p[1];
+            maxRow[y] = Math.max(maxRow[y], x);
+            minRow[y] = Math.min(minRow[y], x);
+            maxCol[x] = Math.max(maxCol[x], y);
+            minCol[x] = Math.min(minCol[x], y);
+        }
+
+        int res = 0;
+        for (int[] p : buildings) {
+            int x = p[0];
+            int y = p[1];
+            if (
+                x > minRow[y] && x < maxRow[y] && y > minCol[x] && y < maxCol[x]
+            ) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args){
+        int[][] buildings = {{1, 2}, {2, 2}, {3, 2}, {2, 1}, {2, 3}};
+        int n=3;
+        CoverBuilding obj = new CoverBuilding();
+        System.out.println(obj.countCoveredBuildings(n, buildings));
+    }
+}
