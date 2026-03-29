d0736be3f872c9716b62075daeba03e98263858d
shramana263 <shramana652@gmail.com>
2026-03-30 01:16:07 +0530

    add implementation for Car class and Solution methods to remove and rearrange cars by ID

---
+++ IPA_practice/IPA35/Solution.java
@@ -0,0 +1,74 @@
+import java.util.Arrays;
+import java.util.Scanner;
+class Car{
+    private int id;
+    private String name, type;
+
+    public Car(int id, String name, String type){
+        this.id=id;
+        this.name=name;
+        this.type=type;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getType(){
+        return type;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Car[] car= new Car[6];
+        for(int i=0;i<6;i++){
+            int a= sc.nextInt();sc.nextLine();
+            String b= sc.nextLine();
+            String c = sc.nextLine();
+
+            car[i]= new Car(a,b,c);
+        }
+        int id= sc.nextInt();sc.nextLine();
+        Car[] obj = RemoveAndRearrange(car, id);
+        if(obj==null){
+            System.out.println("404 not found");
+        }
+        else{
+            for(Car o:obj){
+                System.out.println(o.getId()+":"+o.getName());
+            }
+        }
+        sc.close();
+    }
+
+    public static Car[] RemoveAndRearrange(Car[] car, int id){
+        Car[] obj= new Car[0];
+        int curr, prev;
+        int i=0;
+        while(car[i].getId()!=id){
+            obj=Arrays.copyOf(obj,obj.length+1);
+            obj[obj.length-1]=car[i];
+            i++;
+        }
+        prev=car[i].getId();
+        for(int j=i+1;j<car.length;j++){
+            obj=Arrays.copyOf(obj,obj.length+1);
+            obj[obj.length-1]=car[j];
+            curr=car[j].getId();
+            obj[obj.length-1].setId(prev);
+            prev=curr;
+        }
+        if(obj.length<1){
+            return null;
+        }
+
+        return obj;
+    }
+}
