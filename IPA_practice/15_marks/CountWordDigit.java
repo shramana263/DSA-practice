1b4d1fbcbeb64653a32fcf0a2aae49d6b5396323
shramana263 <shramana652@gmail.com>
2026-04-01 23:27:36 +0530

    add initial implementation of CountWordDigit class to count vowels, consonants, and digits in a string

---
+++ IPA_practice/15_marks/CountWordDigit.java
@@ -0,0 +1,21 @@
+/* Count number of vowel, consonent and digit into a string */
+import java.util.Scanner;
+public class CountWordDigit {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String s= sc.nextLine();
+        int digit=0,vow=0,cons=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=' '){
+                if(Character.isDigit(s.charAt(i))){
+                    digit++;
+                }
+                else{
+                    char c= ;
+                    if(c=='')
+                }
+            }
+        }
+    }
+    
+}
